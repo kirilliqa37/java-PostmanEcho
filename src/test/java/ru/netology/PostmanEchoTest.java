d90f0cdbee7f3344e18fa9d5fdc95d18185e44f2
Кирилл Петров <kirillv1993@yandex.ru>
2022-09-04 16:45:37 +0400

    Postman Echo

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostmanTest(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+    @Test
+    void  shouldCurrentName(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Kirill") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Kirill"))
+        ;
+    }
+}
+
+
+
